6b6c4a37de4d236e445825e2d291e31083ce8934
Jeffrey Peplinski <jeffreypeplinski@jeffreys-mbp.lan>
2022-01-25 16:08:26 -0500

    Got customers of the same zip to display and the edit profile function.

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+
 from . import views
 
 # TODO: Determine what distinct pages are required for the user stories, add a path for each in urlpatterns
@@ -7,5 +8,6 @@ from . import views
 app_name = "employees"
 urlpatterns = [
     path('', views.index, name="index"),
-    path('new/', views.create, name="create")
+    path('new/', views.create, name="create"),
+    path('edit_profile/', views.edit_profile, name="edit_profile")
 ]
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -24,10 +24,14 @@ def index(request):
         logged_in_employee = Employee.objects.get(user=logged_in_user)
 
         today = date.today()
+        # employee_zip = logged_in_employee.zip_code
+        customers_in_my_zipcode = Customer.objects.filter(zip_code = logged_in_employee.zip_code)
+        data_visualization = [item for item in customers_in_my_zipcode]
         
         context = {
             'logged_in_employee': logged_in_employee,
-            'today': today
+            'today': today,
+            'customers_in_my_zipcode': customers_in_my_zipcode
         }
         return render(request, 'employees/index.html', context)
     except ObjectDoesNotExist:
@@ -45,3 +49,22 @@ def create(request):
         return HttpResponseRedirect(reverse('employees:index'))
     else:
         return render(request, 'employees/create.html')
+
+@login_required
+def edit_profile(request):
+    logged_in_user = request.user
+    logged_in_employee = Employee.objects.get(user=logged_in_user)
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        logged_in_employee.name = name_from_form
+        logged_in_employee.address = address_from_form
+        logged_in_employee.zip_code = zip_from_form
+        logged_in_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        context = {
+            'logged_in_employee': logged_in_employee
+        }
+        return render(request, 'employees/edit_profile.html', context)
