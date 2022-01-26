27a47e7238021a385daf367149085586a8c65f76
Bryce Payne <bryce.payne.97@gmail.com>
2022-01-26 15:26:23 -0600

    added descriptive comments to the views

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -10,36 +10,21 @@ from django import forms
 
 
 
-# Create your views here.
-
-# TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
-
 @login_required
 def index(request):
-    # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-
     Customer = apps.get_model('customers.Customer')
     logged_in_user = request.user
     try:
-        # This line will return the customer record of the logged-in user if one exists
         logged_in_employee = Employee.objects.get(user=logged_in_user)
-
-
         weekday_list = [('monday','Monday'), 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
         selected_day = forms.CharField(label='View Day', widget=forms.Select(choices=weekday_list))
         today = date.today()
         weekday = today.strftime('%A')
-        # employee_zip = logged_in_employee.zip_code
         customers_in_my_zipcode = Customer.objects.filter(zip_code = logged_in_employee.zip_code)
         pick_up_day = customers_in_my_zipcode.filter(weekly_pickup = weekday) | customers_in_my_zipcode.filter(one_time_pickup = today)
         suspended_or_no = pick_up_day.exclude(suspend_start__lte = today, suspend_end__gte = today)
         trash_picked_up = suspended_or_no.exclude(date_of_last_pickup = today)
-        
-
-
-        data_visualization = [item for item in suspended_or_no]
-        #put a customer who has a suspension already over
-        #also before today
+        #data_visualization = [item for item in suspended_or_no]
         context = {
             'logged_in_employee': logged_in_employee,
             'today': today,
@@ -52,6 +37,9 @@ def index(request):
         return HttpResponseRedirect(reverse('employees:create'))
 
 
+# confirms if the garbage from the customer has been picked up
+# when the confirm pickup button is clicked it deleted the cusomer and adds a balance
+# to the cusomers account
 @login_required
 def confirm_pickup(request, item_id):
     Customer = apps.get_model('customers.Customer')
@@ -62,7 +50,7 @@ def confirm_pickup(request, item_id):
     pickup_date.save()
     return HttpResponseRedirect(reverse('employees:index'))
 
-
+# searched the customers weekly pickup day and lists them by day and in order of zipcode
 @login_required
 def search_daily_pickups(request, weekday):
     weekday = weekday
@@ -70,7 +58,7 @@ def search_daily_pickups(request, weekday):
     logged_in_employee = Employee.objects.get(user=logged_in_user)
     Customer = apps.get_model('customers.Customer')
     pickup_day = Customer.objects.filter(weekly_pickup = weekday).order_by('zip_code')
-    data_visualization = [item for item in pickup_day]
+    #data_visualization = [item for item in pickup_day]
     context = {
         'logged_in_employee': logged_in_employee,
         'pickup_day': pickup_day,
@@ -91,6 +79,7 @@ def create(request):
     else:
         return render(request, 'employees/create.html')
 
+# gives the employee the ability to edit their profile info
 @login_required
 def edit_profile(request):
     logged_in_user = request.user
